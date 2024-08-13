8fbf5d55e3aa812ce5776400347a5ed28e692492
lunaagustin <lunaagustin727@gmail.com>
2024-08-13 17:07:07 -0300

    cambios en la clase productos y en la clase producto

--- Pagina 2/productos.js
+++ Pagina 2/productos.js
@@ -1,17 +1,27 @@
-let arrayProductos=["Coca Cola","Cocinero Girasol","Pepas Terepin","Rexona M","Ala Ultra Limon","Cepillo Colgate","Colgate Triple Accion","Dulce de Leche","Gallo Oro","Leche Chocolatada","Molito Spaghetti","Pure de Tomate"];
+let arrayProductos=["Coca Cola","Cocinero Girasol","Pepas Terepin","Rexona M","Ala Ultra Limon",
+                    "Cepillo Colgate","Colgate Triple Accion","Dulce de Leche","Gallo Oro",
+                    "Leche Chocolatada","Molito Spaghetti","Pure de Tomate"];
 let arrayStocks= [14,18,7,21,16,11,19,21,30,17,11,8];
 let arrayPrecios=[1400,1750,700,2100,1600,1100,1900,2100,3000,1700,1100,800];
-let arrayImagenes= ["../Pagina 1/Imagenes/Coca Cola.jpg","../Pagina 1/Imagenes/Cocinero Girasol.jpg","../Pagina 1/Imagenes/Pepas Terepin.jpg","../Pagina 1/Imagenes/Rexona M.jpg","../Pagina 2/Imagenes/Ala Ultra Eucalipto Limon.jpg","../Pagina 2/Imagenes/Cepillo Dent Colgate Clasic Medio.jpg","../Pagina 2/Imagenes/Colgate Triple Accion.jpg","../Pagina 2/Imagenes/Dulce de Leche Ilolay.jpg","../Pagina 2/Imagenes/Gallo Oro.jpg","../Pagina 2/Imagenes/Leche Chocolatada Cindor.png","../Pagina 2/Imagenes/Molito Spaghetti.jpg","../Pagina 2/Imagenes/Pure de Tomate La Campagnola.jpg"];
+let arrayImagenes= ["../Pagina 1/Imagenes/Coca Cola.jpg","../Pagina 1/Imagenes/Cocinero Girasol.jpg",
+                    "../Pagina 1/Imagenes/Pepas Terepin.jpg","../Pagina 1/Imagenes/Rexona M.jpg",
+                    "../Pagina 2/Imagenes/Ala Ultra Eucalipto Limon.jpg",
+                    "../Pagina 2/Imagenes/Cepillo Dent Colgate Clasic Medio.jpg",
+                    "../Pagina 2/Imagenes/Colgate Triple Accion.jpg","../Pagina 2/Imagenes/Dulce de Leche Ilolay.jpg",
+                    "../Pagina 2/Imagenes/Gallo Oro.jpg","../Pagina 2/Imagenes/Leche Chocolatada Cindor.png",
+                    "../Pagina 2/Imagenes/Molito Spaghetti.jpg","../Pagina 2/Imagenes/Pure de Tomate La Campagnola.jpg"];
 
-let lista= document.querySelectorAll(".producto");
+let lista= document.querySelector(".productos");
 function ingresarElementos(arrImagenes,arrProductos,arrPrecios,arrStocks){
     for (let i = 0; i < arrProductos.length; i++) {
-        lista[i].innerHTML+= 
-        `<h4 class="bienes">${arrProductos[i]}</h4>
-        <p>Stock: <span class="stock">${arrStocks[i]}</span></p>
-        <p>$<span class="precio">${arrPrecios[i]}</span></p>
-        <img src="${arrImagenes[i]}" alt="${arrProductos[i]}" class="img" />
-        <input type="number" id="cantidad${i}" value="0" min="0" max=${arrStocks[i]}>`
+        lista.innerHTML+= 
+        `<div class="producto">
+            <h4 class="bienes">${arrProductos[i]}</h4>
+            <p>Stock: <span class="stock">${arrStocks[i]}</span></p>
+            <p>$<span class="precio">${arrPrecios[i]}</span></p>
+            <img src="${arrImagenes[i]}" alt="${arrProductos[i]}" class="img" />
+            <input type="number" id="cantidad${i}" value="0" min="0" max=${arrStocks[i]}>
+        </div>`
     }
 }
 ingresarElementos(arrayImagenes,arrayProductos,arrayPrecios,arrayStocks);
